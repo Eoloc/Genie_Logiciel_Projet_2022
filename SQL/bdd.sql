5ff1a7820bce4bc93d401b606d6d3a07dab86bd1
Alexandre Sousa <eoloc57@gmail.com>
2022-05-17 14:52:14 +0200

    Modification de la base de données

--- SQL/bdd.sql
+++ SQL/bdd.sql
@@ -4,21 +4,6 @@ CREATE TABLE Variable (
 	quantiteBorne integer NOT NULL
 );
 
-INSERT INTO Variable VALUES (20.00, 45.00, 20);
-
-CREATE TABLE Vehicule (
-	immatriculation varchar(20) PRIMARY KEY,
-	marque varchar(50) NOT NULL,
-	modele varchar(50) NOT NULL,
-	couleur varchar(50) NOT NULL,
-	idcompte serial,
-	statusAssociation varchar(50),
-	FOREIGN KEY (idCompte) REFERENCES Compte (idCompte),
-);
-
-INSERT INTO Vehicule VALUES ('AA123AA', 'Mercedes Benz', 'CLA', 'Noir', 1,'Permanent');
-INSERT INTO Vehicule VALUES ('BB456BB', 'Nissan', 'Micra', 'Vert' NULL, NULL);
-
 CREATE TABLE Compte (
 	idCompte serial PRIMARY KEY,
 	email varchar(255) UNIQUE NOT NULL,
@@ -28,53 +13,43 @@ CREATE TABLE Compte (
 	age integer NOT NULL,
 	estClient bool NOT NULL,
 	estGerant bool NOT NULL,
-	estAdministrateur bool NOT NULL,
+	estAdministrateur bool NOT NULL
 );
 
-INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur, immatriculation) VALUES ('alexandre@gmail.com', '123', 'Sousa', 'Alexandre', 21, TRUE, FALSE, FALSE, 'AA123AA');
-INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur, immatriculation) VALUES ('jean@gmail.com', '456', 'Mathieu', 'Jean', 21, TRUE, FALSE, FALSE, NULL);
-INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur, immatriculation) VALUES ('admin@gmail.com', '123456', 'Drommer', 'Fabien', 21, FALSE, FALSE, TRUE, NULL);
-INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur, immatriculation) VALUES ('gerant@gmail.com', '1', 'Zarrad', 'Youssef', 21, FALSE, TRUE, FALSE, NULL);
+CREATE TABLE Vehicule (
+	immatriculation varchar(20) PRIMARY KEY,
+	marque varchar(50) NOT NULL,
+	modele varchar(50) NOT NULL,
+	couleur varchar(50) NOT NULL
+);
+
+CREATE TABLE VehiculeAssociation (
+	immatriculation varchar(20),
+	idcompte serial,
+	statusAssociation varchar(50),
+	PRIMARY KEY (immatriculation, idcompte),
+	FOREIGN KEY (immatriculation) REFERENCES Vehicule (immatriculation),
+	FOREIGN KEY (idCompte) REFERENCES Compte (idCompte)
+);
 
 CREATE TABLE Borne (
 	idBorne serial PRIMARY KEY,
 	etatBorne varchar(50) NOT NULL
 );
 
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('Disponible');
-INSERT INTO Borne (etatBorne) VALUES ('En panne');
-INSERT INTO Borne (etatBorne) VALUES ('En panne');
-INSERT INTO Borne (etatBorne) VALUES ('En panne');
-
 
 CREATE TABLE Reservation (
 	idReservation serial PRIMARY KEY,
-	dateDebut date NOT NULL,
-	dateFin date NOT NULL,
+	dateDebut timestamp NOT NULL,
+	dateFin timestamp NOT NULL,
 	etatReservation varchar(50) NOT NULL,
-	prixTotal decimal(10,2) NOT NULL,
 	idCompte serial NOT NULL,
 	idBorne serial NOT NULL,
 	estPermanente bool NOT NULL,
 	FOREIGN KEY (idCompte) REFERENCES Compte (idCompte),
 	FOREIGN KEY (idBorne) REFERENCES Borne (idBorne)
 );
+
 
 CREATE TABLE HistoriqueDepense (
 	idCompte serial,
@@ -84,3 +59,40 @@ CREATE TABLE HistoriqueDepense (
 	FOREIGN KEY (idReservation) REFERENCES Reservation (idReservation),
 	PRIMARY KEY(idCompte, idReservation)
 );
+
+INSERT INTO Variable VALUES (20.00, 45.00, 20);
+
+INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur) VALUES ('alexandre@gmail.com', '123', 'Sousa', 'Alexandre', 21, TRUE, FALSE, FALSE);
+INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur) VALUES ('jean@gmail.com', '456', 'Mathieu', 'Jean', 21, TRUE, FALSE, FALSE);
+INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur) VALUES ('admin@gmail.com', '123456', 'Drommer', 'Fabien', 21, FALSE, FALSE, TRUE);
+INSERT INTO Compte (email, mdp, nom, prenom, age, estClient, estGerant, estAdministrateur) VALUES ('gerant@gmail.com', '1', 'Zarrad', 'Youssef', 21, FALSE, TRUE, FALSE);
+
+INSERT INTO Vehicule VALUES ('AA123AA', 'Mercedes Benz', 'CLA', 'Noir');
+INSERT INTO Vehicule VALUES ('BB456BB', 'Nissan', 'Micra', 'Vert');
+
+INSERT INTO VehiculeAssociation VALUES ('AA123AA', 1, 'Permanent');
+INSERT INTO VehiculeAssociation VALUES ('BB456BB', 1, 'Permanent');
+INSERT INTO VehiculeAssociation VALUES ('BB456BB', 2, 'Temporaire');
+
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('Disponible');
+INSERT INTO Borne (etatBorne) VALUES ('En panne');
+INSERT INTO Borne (etatBorne) VALUES ('En panne');
+INSERT INTO Borne (etatBorne) VALUES ('En panne');
+
+INSERT INTO Reservation (dateDebut, dateFin, etatReservation, idCompte, idBorne, estPermanente) VALUES ('2022-05-22 15:00:00', '2022-05-22 16:00:00', 'En attente', 1, 1, FALSE);
