a4f45b37708fc0fd49777b8085f5bb78d71bfaa7
Alexandre Sousa <eoloc57@gmail.com>
2022-05-27 15:26:58 +0200

    Ajout de la création de la réservation

---
+++ src/main/java/fr/ul/miage/Genie_Logiciel_Projet_2022/controller/ReservationController.java
@@ -0,0 +1,8 @@
+package fr.ul.miage.Genie_Logiciel_Projet_2022.controller;
+
+public class ReservationController {
+    private DatabaseController bdd;
+
+    public ReservationController(DatabaseController bd) { bdd = bd; }
+
+}
